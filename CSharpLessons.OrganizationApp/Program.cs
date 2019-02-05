77a13ea11f144779a4b5b0369e9e2194b49ae88d
kudriako <kudriako@gmail.com>
2019-02-05 12:58:33 +0200

    Lesson 1. Methods. - Added boundary methods.

--- CSharpLessons.OrganizationApp/Program.cs
+++ CSharpLessons.OrganizationApp/Program.cs
@@ -2,11 +2,24 @@
 
 namespace CSharpLessons.OrganizationApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Start();
+            End();
+        }
+
+        static void Start()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void End()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
         }
     }
 }
