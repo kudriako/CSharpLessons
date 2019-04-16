8a5b02b6727436b73f9736e5c2738ca4ef7aef20
Kudriavtsev, Kostiantyn <kostiantyn.kudriavts@ge.com>
2019-04-16 15:24:37 +0300

    Lesson 12. Added tasks project.

---
+++ lesson-12/CSharpLessons.Tasks/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpLessons.Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
