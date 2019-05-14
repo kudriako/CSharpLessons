80baea71c7b95c0f99085395ccdb548e719e975a
Kudriavtsev, Kostiantyn <kostiantyn.kudriavts@ge.com>
2019-05-14 13:12:14 +0300

    Lesson 13. Unit testing, TDD and tools Added calculator project and tests.

---
+++ lesson-13/CSharpLessons.Fractions.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSharpLessons.Fractions.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSharpLessons.Fractions.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c2260dd6-1d3c-471f-a52d-3df17310e426")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ lesson-13/CSharpLessons.Fractions/IFractionCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLessons.Fractions
+{
+    public interface IFractionCalculator
+    {
+        Fraction Add(Fraction a, Fraction b);
+
+        Fraction Subtract(Fraction a, Fraction b);
+
+        Fraction Multiply(Fraction a, Fraction b);
+
+        Fraction Divide(Fraction a, Fraction b);
+    }
+}
